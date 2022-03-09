a2718d543c3055d488055b4f43dd28de101339f9
Navnita <72120403+nandakn1@users.noreply.github.com>
2022-03-09 11:28:58 +1100

    Fix/sysld crashing (#69)
    * return empty if no nodes or links
    
    * build sysld
    
    * bump version

--- extension/plugins/sysld/diagram.ts
+++ extension/plugins/sysld/diagram.ts
@@ -3,7 +3,7 @@ import { DiagramModel, DiagramObjectData } from "../../views/diagram/model";
 export async function buildModel(module: any): Promise<DiagramModel> {
   const nodes: DiagramObjectData[] = [];
   const edges: DiagramObjectData[] = [];
-  if (module.nodes.length && module.links.length) {
+  if (module.nodes?.length && module.links?.length) {
     return {
       nodes: module.nodes,
       edges: module.links
